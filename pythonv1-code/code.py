a0154d2e47f7320859b17967f5a8c9768a6f4bda
Robert Stave <robstave@yahoo.com>
2023-07-16 14:56:29 -0500

    Initial upload

---
+++ pythonv1-code/code.py
@@ -0,0 +1,223 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+"""CircuitPython LFO."""
+import time
+import board
+import busio
+import random
+import neopixel
+import adafruit_mcp4728
+from digitalio import DigitalInOut, Direction
+from analogio import AnalogIn
+
+# gate output pin
+gate = DigitalInOut(board.A2)
+gate.direction = Direction.OUTPUT
+
+
+
+analog_in = AnalogIn(board.A0)
+freq_in = AnalogIn(board.A1)
+
+# pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+pixel = neopixel.NeoPixel(board.A3, 1)
+
+pixel.brightness = 0.3
+
+
+            
+
+last_time_read = time.monotonic_ns()
+last_time_write = time.monotonic_ns()
+
+write_len = 800000
+read_len = 50000000
+
+
+gate.value = True
+
+
+
+
+#  i2c setup
+# i2c = board.I2C()  
+i2c = busio.I2C(board.SCL, board.SDA)
+# uses board.SCL and board.SDA
+ 
+pixel.fill(0x00FF00)
+pixel.show()
+
+#  dac setup over i2c
+# could be 64
+dac = adafruit_mcp4728.MCP4728(i2c, 0x60)
+#  dac raw value (12 bit)
+# dac.raw_value = 4095
+
+accumulator1 = 0
+accumulator2 = 0
+accumulator3 = 0
+accumulator4 = 0
+
+tuneword4 = 1000
+tuneword3 = 1000
+tuneword2 = 9000
+tuneword1 = 8000
+
+waveform = 2
+
+
+def mapRange(x, in_min, in_max, out_min, out_max):
+    """Map an input x from range (in_min, in_max) to range (out_min, out_max).  This
+    is a Python implementation of the Arduino map() function.  Works on either
+    integers or floating point.
+    """
+    divisor = in_max - in_min
+    
+    if divisor == 0:
+        return out_min
+    else:
+        return ((x - in_min) * (out_max - out_min) / divisor) + out_min
+        
+while True:
+    
+    now = time.monotonic_ns()
+    
+    if (now - last_time_read) > read_len:
+        
+        f = freq_in.value
+        rootFreq = mapRange(freq_in.value, 0, 65535, 50, 9200)
+        tuneword1 = int(rootFreq)
+        tuneword2 = int(rootFreq * 0.88)
+        tuneword3 = int(rootFreq * 0.62)
+        tuneword4 = int(rootFreq * 0.45)
+        
+        v = analog_in.value
+        x = mapRange(analog_in.value, 0, 65535, 1, 7)
+        
+        waveform = int(x)
+
+        if (waveform == 1):
+            pixel.fill(0xFF0000)
+            pixel.show()
+        
+        if waveform == 2:
+            pixel.fill(0x00FF00)
+            pixel.show()
+            
+        if waveform == 3:
+            pixel.fill(0x0000FF)
+            pixel.show()
+
+        if waveform == 4:
+            pixel.fill(0xFF00FF)
+            pixel.show()
+            
+        if waveform == 5:
+            pixel.fill(0xFFFF00)
+            pixel.show()
+            
+        if waveform == 6:
+            pixel.fill(0x00FFFF)
+            pixel.show()
+
+        last_time_read = now
+
+    if (now - last_time_write) > write_len:
+        
+        accumulator1 = accumulator1 + tuneword1
+        accumulator2 = accumulator2 + tuneword2
+        accumulator3 = accumulator3 + tuneword3
+        accumulator4 = accumulator4 + tuneword4
+
+        mask = 0xfffff
+        
+        val1 = ((accumulator1 & mask) >> 8) 
+        val2 = ((accumulator2 & mask) >> 8) 
+        val3 = ((accumulator3 & mask) >> 8) 
+        val4 = ((accumulator4 & mask) >> 8) 
+
+
+# ramp
+        if waveform == 1:        
+            dac.channel_a.raw_value = val1 
+            dac.channel_b.raw_value = val2 
+            dac.channel_c.raw_value = val3 
+            dac.channel_d.raw_value = val4 
+
+# ramp down
+        if waveform == 2:
+            dac.channel_a.raw_value = 4095 - val1  
+            dac.channel_b.raw_value = 4095 - val2
+            dac.channel_c.raw_value = 4095 - val3 
+            dac.channel_d.raw_value = 4095 - val4 
+       
+# triangle
+        if waveform == 3:
+            
+            if val1 > 2048:
+                val1 = 4095 - val1
+                
+            dac.channel_a.raw_value = (val1 << 1 & 0xfff) 
+            
+            if val2 > 2048:
+                val2 = 4095 - val2
+                
+            dac.channel_b.raw_value = (val2 << 1 & 0xfff) 
+        
+            if val3 > 2048:
+                val3 = 4095 - val3
+                
+            dac.channel_c.raw_value = (val3 << 1 & 0xfff) 
+       
+            if val4 > 2048:
+                val4 = 4095 - val4
+                
+            dac.channel_d.raw_value = (val4 << 1 & 0xfff)       
+         
+# square
+        if waveform == 4:
+            
+            if val1 < 2048:
+                dac.channel_a.raw_value = 0
+            else:
+                dac.channel_a.raw_value = 4095
+                
+            if val2 < 2048:
+                dac.channel_b.raw_value = 0
+            else:
+                dac.channel_b.raw_value = 4095
+                
+            if val3 < 2048:
+                dac.channel_c.raw_value = 0
+            else:
+                dac.channel_c.raw_value = 4095
+                
+            if val4 < 2048:
+                dac.channel_d.raw_value = 0
+            else:
+                dac.channel_d.raw_value = 4095
+        
+#  90 deg ramps        
+        if waveform == 5:
+           
+            dac.channel_a.raw_value = ((val1) & 0xfff) 
+            dac.channel_b.raw_value = ((val1 - 1024) & 0xfff)
+            dac.channel_c.raw_value = ((val1 - 2048) & 0xfff)
+            dac.channel_d.raw_value = ((val1 - 3072) & 0xfff) 
+            
+        if waveform == 6:
+
+            if (val1 == 0) or (val1 == 2047) or (val1 == 1024) or (val1 == 3075):
+                dac.channel_a.raw_value = random.randrange(0, 4095)
+           
+            if (val2 == 0) or (val2 == 2047) or (val2 == 1024) or (val2 == 3075):
+                dac.channel_b.raw_value = random.randrange(0, 4095)
+            if (val3 == 0) or (val3 == 2047) or (val3 == 1024) or (val3 == 3075):
+                dac.channel_c.raw_value = random.randrange(0, 4095)
+
+            if (val4 == 0) or (val4 == 2047) or (val4 == 1024) or (val4 == 3075):
+                dac.channel_d.raw_value = random.randrange(0, 4095)				
+ 
+        last_time_write = now
+        
+  
--- readme.md
+++ readme.md
@@ -1,2 +1,27 @@
 # QTPY LFO
 
+Inspired by Ochd
+
+https://divkidvideo.com/ochd-the-second-divkid-eurorack-module/
+
+This only has 4, and was more of a means to build something with a DAC and the RP2040.  I would say if you want 8 LFOs, get that, otherwise this could fit some similar tickboxes.
+
+This "journey" has 4 parts, so do not consider this a standalone project that you can just build from.  Im not supporting this, just taking notes.
+
+The First schematic is what I do in easyeda.  I generally sketch out the idea and then split it into two parts.  Top and Bottom.
+
+The Second pass had some bugs.  Mostly the pots were hooked up to 5v instead of 3.3.  Opps.  That was the run I made and I fixed it on the fly.
+
+The Main schematics should be considered untested, but if I were to do another run, this might be what I use.
+
+The final arduino code would be for a schematic that does not exist.  I would say the final incarnation would be to ditch the op amp and 4 leds and just use neopixels.  The code is there for that. It works on a bread board.
+
+the design really in that case would just be the QT, DAC, some pull up resistors and thats about it.
+
+I will link to the project and add the final gerbers, but keep in mind, I have not built from the final gerbers. They are as is.
+
+And I really would like to do this again without the LEDs and just use neopixels.  The code for that is same circuit python directory/lib, but just swap out the code in python-vx.
+
+
+
+
