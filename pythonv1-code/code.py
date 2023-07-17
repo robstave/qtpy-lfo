2e1f84f7c7b12a9968f0ee6b0a66bf1ca5f0908b
Robert Stave <robstave@yahoo.com>
2023-07-17 10:06:02 -0500

    fix

--- pythonv1-code/code.py
+++ pythonv1-code/code.py
@@ -10,11 +10,6 @@ import adafruit_mcp4728
 from digitalio import DigitalInOut, Direction
 from analogio import AnalogIn
 
-# gate output pin
-gate = DigitalInOut(board.A2)
-gate.direction = Direction.OUTPUT
-
-
 
 analog_in = AnalogIn(board.A0)
 freq_in = AnalogIn(board.A1)
@@ -22,10 +17,7 @@ freq_in = AnalogIn(board.A1)
 # pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
 pixel = neopixel.NeoPixel(board.A3, 1)
 
-pixel.brightness = 0.3
-
-
-            
+pixel.brightness = 0.1
 
 last_time_read = time.monotonic_ns()
 last_time_write = time.monotonic_ns()
@@ -33,23 +25,20 @@ last_time_write = time.monotonic_ns()
 write_len = 800000
 read_len = 50000000
 
+# gate output pin
+# gate = DigitalInOut(board.A2)
+# gate.direction = Direction.OUTPUT
 
-gate.value = True
-
-
-
-
+ 
 #  i2c setup
 # i2c = board.I2C()  
 i2c = busio.I2C(board.SCL, board.SDA)
 # uses board.SCL and board.SDA
  
-pixel.fill(0x00FF00)
-pixel.show()
 
 #  dac setup over i2c
-# could be 64
-dac = adafruit_mcp4728.MCP4728(i2c, 0x60)
+dac = adafruit_mcp4728.MCP4728(i2c, 0x64)
+# gate.value = True
 #  dac raw value (12 bit)
 # dac.raw_value = 4095
 
@@ -65,7 +54,9 @@ tuneword1 = 8000
 
 waveform = 2
 
-
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
+    
 def mapRange(x, in_min, in_max, out_min, out_max):
     """Map an input x from range (in_min, in_max) to range (out_min, out_max).  This
     is a Python implementation of the Arduino map() function.  Works on either
@@ -92,32 +83,37 @@ while True:
         tuneword4 = int(rootFreq * 0.45)
         
         v = analog_in.value
-        x = mapRange(analog_in.value, 0, 65535, 1, 7)
+        x = mapRange(analog_in.value, 0, 65535, 1, 8)
         
         waveform = int(x)
 
         if (waveform == 1):
-            pixel.fill(0xFF0000)
+            pixel.fill(0x420000)
+            pixel.brightness = 0.1
             pixel.show()
         
         if waveform == 2:
-            pixel.fill(0x00FF00)
+            pixel.fill(0x004200)
             pixel.show()
             
         if waveform == 3:
-            pixel.fill(0x0000FF)
+            pixel.fill(0x000042)
             pixel.show()
 
         if waveform == 4:
-            pixel.fill(0xFF00FF)
+            pixel.fill(0x420042)
             pixel.show()
             
         if waveform == 5:
-            pixel.fill(0xFFFF00)
+            pixel.fill(0x424200)
             pixel.show()
             
         if waveform == 6:
-            pixel.fill(0x00FFFF)
+            pixel.fill(0x004242)
+            pixel.show()
+            
+        if waveform == 7:
+            pixel.fill(0x424242)
             pixel.show()
 
         last_time_read = now
@@ -207,16 +203,31 @@ while True:
             
         if waveform == 6:
 
-            if (val1 == 0) or (val1 == 2047) or (val1 == 1024) or (val1 == 3075):
-                dac.channel_a.raw_value = random.randrange(0, 4095)
-           
-            if (val2 == 0) or (val2 == 2047) or (val2 == 1024) or (val2 == 3075):
-                dac.channel_b.raw_value = random.randrange(0, 4095)
-            if (val3 == 0) or (val3 == 2047) or (val3 == 1024) or (val3 == 3075):
-                dac.channel_c.raw_value = random.randrange(0, 4095)
+            if val1 > 2048:
+                val1 = 4095 - val1
+                
+            if val2 > 2048:
+                val2 = 4095 - val2
 
-            if (val4 == 0) or (val4 == 2047) or (val4 == 1024) or (val4 == 3075):
-                dac.channel_d.raw_value = random.randrange(0, 4095)				
+            if val3 > 2048:
+                val3 = 4095 - val3
+                
+            if val4 > 2048:
+                val4 = 4095 - val4 
+            
+            
+            dac.channel_a.raw_value = ((val1  + val2) & 0xfff)
+
+            dac.channel_b.raw_value = ((val2  + max(0, val3 - (val4 >> 2))) & 0xfff)
+            dac.channel_c.raw_value = ((val3  + max(0, val4 - (val1 >> 2))) & 0xfff)
+            dac.channel_d.raw_value = ((val1  + val4) & 0xfff)
+
+        if waveform == 7:
+            dac.channel_a.raw_value = (((val1  + val2)) >> 1 & 0xfff)
+
+            dac.channel_b.raw_value = (((val2  + 4095 - val4)) >> 1 & 0xfff)
+            dac.channel_c.raw_value = (((val3  + val1)) >> 1 & 0xfff)
+            dac.channel_d.raw_value = (((val4  + 4095 - val3)) >> 1 & 0xfff)			
  
         last_time_write = now
         
